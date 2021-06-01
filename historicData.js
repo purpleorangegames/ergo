0b3ec4043d9ce16a47d146d3b3ee69e9b524985b
tiago <tiagocc0@gmail.com>
2021-06-01 12:47:13 -0300

    "Automatic commit from Tiago's QtErgoPoolCMD."

--- historicData.js
+++ historicData.js
@@ -17,3 +17,7 @@ ProportionalReward = [
 TotalPerBlock = [ 
 
  ];
+
+Workers = [ 
+
+{'ID': '1','worker': 'MainRig','timestamp': '1622562335','lastBeat': '1622562257','mode': 'solo','offline': 'false','hr': '370671191.58','hr2': '345809797.48806834','hr3': '345809797.48806834'},{'ID': '2','worker': 'TiagoDesk2','timestamp': '1622562335','lastBeat': '1622562258','mode': 'solo','offline': 'false','hr': '223154056.2096','hr2': '195486560.95803997','hr3': '195486560.95803997'},{'ID': '3','worker': 'TiagoDesk1','timestamp': '1622562335','lastBeat': '1622562267','mode': 'solo','offline': 'false','hr': '127500000','hr2': '117124357.88700001','hr3': '117124357.88700001'},{'ID': '4','worker': 'mariominer','timestamp': '1622562335','lastBeat': '1622562225','mode': 'solo','offline': 'false','hr': '1462000000','hr2': '1169033333.3333333','hr3': '1169033333.3333333'},{'ID': '5','worker': 'Tweenky','timestamp': '1622562335','lastBeat': '1622562266','mode': 'solo','offline': 'false','hr': '326400000','hr2': '240676610.26174253','hr3': '240676610.26174253'},{'ID': '6','worker': 'Mario2','timestamp': '1622562335','lastBeat': '1622562209','mode': 'solo','offline': 'false','hr': '543923743.083','hr2': '321691094.70898336','hr3': '321691094.70898336'},{'ID': '7','worker': 'TiagoDesk3','timestamp': '1622562335','lastBeat': '1622562075','mode': 'solo','offline': 'false','hr': '156890786.35520002','hr2': '96555269.0797143','hr3': '96555269.0797143'} ];
